64d0489d6566e6aa102dfa5d4b582081e3edba25
Jason McKee <jasonmckee@live.com>
2022-09-29 17:40:47 -0500

    make About be responsive

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import { Grid, Box, Typography } from "@mui/material";
 
 function Item(props) {
   const { sx, ...other } = props;
@@ -42,44 +41,44 @@ Item.propTypes = {
 
 export default function About() {
   return (
-    <div style={{ width: "100%" }}>
-      <Box sx={{ bgcolor: "#00276c" }}>
-        <Typography
-          variant="h3"
-          align="center"
-          color="white"
-          paragraph
-          sx={{ pt: 4 }}
-        >
-          What Is Ribbit?
-        </Typography>
-        <Box
-          sx={{
-            display: "grid",
-            bgcolor: "#00276C",
-            gridTemplateColumns: "repeat(4, 1fr)",
-          }}
-        >
+    <Box sx={{ bgcolor: "#00276c" }}>
+      <Typography
+        variant="h3"
+        align="center"
+        color="white"
+        paragraph
+        sx={{ pt: 4 }}
+      >
+        What Is Ribbit?
+      </Typography>
+      <Grid container spacing={3} alignItems="center" justifyContent="center">
+        <Grid item xs="12" sm="6" md="2">
           <Item>
             Ribbit is a novel digital text messaging mode for VHF/UHF
             communications for recreational and emergency use which radically
             increases the density of information transmitted by spectrum used.
           </Item>
+        </Grid>
+        <Grid item xs="12" sm="6" md="2">
           <Item>
             It leverages the computing power of the modern smartphone to
             increase the capabilities of any Handy Talkie without requiring any
             additional hardware.
           </Item>
+        </Grid>
+        <Grid item xs="12" sm="6" md="2">
           <Item>
             Its redundant distributed nature allows it to function even when
             internet connectivity is lost during emergencies.
           </Item>
+        </Grid>
+        <Grid item xs="12" sm="6" md="2">
           <Item>
             Ribbit is open source and currently in its early stages of
             development.
           </Item>
-        </Box>
-      </Box>
-    </div>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
