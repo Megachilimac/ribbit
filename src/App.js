353b45cf08da5c04ee697e7a0fe6ce9fe8fa0074
Jason McKee <jasonmckee@live.com>
2022-10-12 10:45:06 -0500

    move menu to component. add Home to menu

--- src/App.js
+++ src/App.js
@@ -1,40 +1,30 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import Toolbar from "@mui/material/Toolbar";
-import { MenuItem, Typography } from "@mui/material";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Menu from "./components/Menu";
 
 const theme = createTheme();
 
-const scrollDown = (ref) => {
-  ref.current?.scrollIntoView({ behavior: "smooth" });
-};
-
 function App() {
+  const home = React.useRef(null);
   const about = React.useRef(null);
   const contact = React.useRef(null);
-  const section = [
+  const sections = [
+    { displayName: "Home", sectionRef: home },
     { displayName: "About", sectionRef: about },
     { displayName: "Contact Us", sectionRef: contact },
   ];
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar position="relative" sx={{ bgcolor: "#010638" }}>
-        <Toolbar>
-          {section.map((page) => (
-            <MenuItem key={page} onClick={() => scrollDown(page.sectionRef)}>
-              <Typography textAlign="center">{page.displayName}</Typography>
-            </MenuItem>
-          ))}
-        </Toolbar>
-      </AppBar>
+      <Menu menuSections={sections} />
       <main>
-        <Hero />
+        <div ref={home}>
+          <Hero />
+        </div>
         <div ref={about}>
           <About />
         </div>
---
+++ src/components/Menu.js
@@ -0,0 +1,22 @@
+import { AppBar, Toolbar, MenuItem, Typography } from "@mui/material";
+
+const scrollDown = (ref) => {
+  ref.current?.scrollIntoView({ behavior: "smooth" });
+};
+
+export default function Menu({ menuSections }) {
+  return (
+    <AppBar position="relative" sx={{ bgcolor: "#010638" }}>
+      <Toolbar>
+        {menuSections.map((page) => (
+          <MenuItem
+            key={page.displayName}
+            onClick={() => scrollDown(page.sectionRef)}
+          >
+            <Typography textAlign="center">{page.displayName}</Typography>
+          </MenuItem>
+        ))}
+      </Toolbar>
+    </AppBar>
+  );
+}
