a9c1ed0b705035372bcebab88da724ee73af25db
Jason McKee <jasonmckee@live.com>
2023-02-02 09:10:16 -0600

    add additional video links

--- src/components/Learn.js
+++ src/components/Learn.js
@@ -3,7 +3,7 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme();
 
-theme.typography.h3 = {
+theme.typography.h4 = {
   fontSize: "1.2rem",
   "@media (min-width:600px)": {
     fontSize: "1.5rem",
@@ -39,23 +39,35 @@ export default function Learn() {
             }}
           >
             <ListItemText>
-              <Typography variant="h3" color="white">
-                View the
+              <Typography variant="h4" color="white">
+                View the{" "}
                 <a href="RibbitPoster-July2022.pdf" download>
                   DEFCON Aug 2022 Poster
                 </a>
               </Typography>
             </ListItemText>
             <ListItemText>
-              <Typography variant="h3" color="white">
-                View the QSOToday Sept 2022 presentation on
+              <Typography variant="h4" color="white">
+                View the QSOToday Sept 2022 presentation on{" "}
                 <a href="https://youtu.be/_jN4IVccIEw">YouTube</a>
               </Typography>
             </ListItemText>
             <ListItemText>
-              <Typography variant="h3" color="white">
-                View the RATPAC Nov 2022 presentation on
+              <Typography variant="h4" color="white">
+                View the RATPAC Nov 2022 presentation on{" "}
                 <a href="https://youtu.be/TGzgIjEt9wA">YouTube</a>
+              </Typography>
+            </ListItemText>
+            <ListItemText>
+              <Typography variant="h4" color="white">
+                Understand how it works with HB9BLA presentation on{" "}
+                <a href="https://youtu.be/ubPP48ojJ3E">YouTube</a>
+              </Typography>
+            </ListItemText>
+            <ListItemText>
+              <Typography variant="h4" color="white">
+                Rattlegram demo and tutorial by Ahmet on{" "}
+                <a href="https://youtu.be/0jtzA3alpuw">YouTube</a>
               </Typography>
             </ListItemText>
           </List>
